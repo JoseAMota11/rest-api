b2fd146a5f9d19af419ab855a968593af4fa1df2
José Mota <jose.a.mota11@gmail.com>
2023-08-16 14:46:49 -0400

    Solving CORS issues

--- src/index.js
+++ src/index.js
@@ -2,23 +2,10 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT;
-
-const whitelist = ['http://localhost:4040'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+const PORT = process.env.PORT ?? 6060;
 
 // Middleware
-app.get('/movies', cors(corsOptions), function (req, res, next) {
-  res.json({ message: 'This is CORS-enabled for a whitelisted domain.' });
-});
+app.use(cors());
 
 app.get('/movies', (req, res) => {
   res.json({ message: 'Welcome to the API' });
